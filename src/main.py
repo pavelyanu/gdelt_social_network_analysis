fe37d128394f5c4826b9f44a7a4a801af9c8045d
yanushop <pyanushonak@gmail.com>
2024-12-16 12:49:35 +0100

    Added showcase in main.py

--- src/main.py
+++ src/main.py
@@ -1,8 +1,9 @@
 from argparse import ArgumentParser, Namespace
 
+from src.analysis import *
 from src.data import *
-from src.network import gdelt_network_vanilla
-from src.plot import plot_gdelt_network
+from src.network import *
+from src.plot import *
 
 parser = ArgumentParser()
 
@@ -14,11 +15,27 @@ def main(args: Namespace):
     migration = get_migration(args.migration_path)
     refugee = get_refugee(args.refugee_path)
     gdelt = get_gdelt(args.gdelt_path)
+    gdelt.clean_data()
 
-    G = gdelt_network_vanilla(gdelt)
-    plot_gdelt_network(G)
+    G_2020 = gdelt_network_vanilla(gdelt, 2020)
 
+    centralities = compute_basic_centralities(G_2020)
+    communities = compute_communities(G_2020)
+    summary = summarize_network(G_2020)
+    print(summary)
 
+    plot_top_centrality_nodes(centralities, 'degree_centrality', top_n=10)
+    plot_top_centrality_nodes(centralities, 'betweenness', top_n=10)
+    plot_top_centrality_nodes(centralities, 'closeness', top_n=10)
+    plot_top_centrality_nodes(centralities, 'eigenvector', top_n=10)
+    plot_communities(G_2020, communities)
+
+    years = range(1990, 2001)
+    all_networks = gdelt_network_vanilla(gdelt, years)
+    time_summary = temporal_analysis(all_networks)
+    plot_network_summary_over_time(time_summary)
+
+    plot_network_with_year_slider(all_networks)
 
 if __name__ == '__main__':
     args: Namespace = parser.parse_args()
